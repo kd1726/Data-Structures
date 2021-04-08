8d88b1e86a5216eb9ceaf1a7989b06979c6acbe6
kd1726 <kd1726@nyu.edu>
2021-04-07 21:02:11 -0400

    Dynamic Array

--- Arrays/Dynamic_Arrays.py
+++ Arrays/Dynamic_Arrays.py
@@ -9,7 +9,7 @@ class DynamicList:
         return self.n
 
     def get_item(self,index):
-        if not 0>index>self.size:
+        if not 0<index<self.size:
             return "IndexError(Index is out of range)"
         return self.array[index]
 
@@ -19,11 +19,11 @@ class DynamicList:
         self.array[self.n]=element
         self.n+=1
         return
-        
-    def pop():
+
+    def pop(self):
         self.array[self.n]=None
         self.n-=1
-        return display()
+        return self.display()
 
     def _resize(self,new_size):
         new_arr = self._make_array(new_size)
